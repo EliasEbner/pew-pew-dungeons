14fdf02f960f87673cf8008057e19156512807cf
Elias Ebner <eliase229@gmail.com>
2025-05-16 02:36:22 +0200

    added camera class

---
+++ src/main/java/com/pewpewdungeons/core/Camera.java
@@ -0,0 +1,12 @@
+package com.pewpewdungeons.core;
+
+import com.pewpewdungeons.physics.Vector2;
+
+public class Camera {
+
+    public Vector2 position;
+
+    public Camera(Vector2 position) {
+        this.position = position;
+    }
+}
